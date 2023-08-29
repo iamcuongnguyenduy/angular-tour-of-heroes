a2885707965aeb91c13039515a03898605ca9a58
cnguyenduy <cnguyenduyavaya.com>
2023-08-29 17:42:20 +0700

    commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  // heroes = HEROES;
+  heroes: Hero[]=[];
+ 
+  constructor(private heroService: HeroService){
+  }
+
+  getHeroes(){
+    this.heroes = this.heroService.getHeroes()
+  }
+
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+
+
 }
